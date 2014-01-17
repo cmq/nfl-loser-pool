b691b0995e0ad7b3ba5aa39c5821896600ffac62
Kirk Hemmen <kirk.hemmen@gmail.com>
2014-01-16 21:37:04 -0600

    - Installed Yii framework and created an app - Modified the configuration to include an external settings file and set up the database and environment-aware settings (similar to how I did dough) - Tested the database connection in index.php

---
+++ src/root/protected/config/main.php
@@ -0,0 +1,109 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+$_config = array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'NFL Loser Pool',
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+	),
+
+	'modules'=>array(
+		// uncomment the following to enable the Gii tool
+		/*
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'Enter Your Password Here',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+		),
+		*/
+	),
+
+	// application components
+	'components'=>array(
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
+		),
+		/*
+		 * dough version
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
+	        'class'=>'WebUser',    // use /protected/components/WebUser.php instead of the default -- allows us to refresh data from the DB
+		),
+		 */
+		// uncomment the following to enable URLs in path-format
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'showScriptName'=>true,    // in production, this is false
+			'rules'=>array(
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+		),
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=loserpool2',
+			'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => 's0methIng',
+			'charset' => 'utf8',
+		    'enableParamLogging' => true,
+		),
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>'site/error',
+		),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
+			),
+		),
+	),
+
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>include(dirname(__FILE__).'/settings.php'),
+);
+
+
+/**
+ * Modify settings based on the environment
+ */
+// PRODUCTION
+if (strstr(strtolower($_SERVER['HTTP_HOST']), 'loserpool.kdhstuff.com') !== false) {
+    // Apache!  Hide index.php script name
+    $_config['components']['urlManager']['showScriptName'] = false;
+    // Change database connection information
+    $_config['components']['db'] = array(
+		'connectionString' => 'mysql:host=localhost;dbname=kdhstuff_loserpool2',
+		'emulatePrepare' => true,
+		'username' => 'kdhstuff_dough',
+		'password' => 'lRKE2@#y9D1+',
+		'charset' => 'utf8',
+	);
+}
+
+return $_config;
---
+++ src/root/protected/config/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * site-wide settings
+ * 
+ * acces with param('$KEY')
+ */
+
+return array(
+    'adminEmail'                  => 'kirk.hemmen@gmail.com',
+);

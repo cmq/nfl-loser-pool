550e5cf4e9887597425740ceb43b4d304d416a6b
Kirk Hemmen <kirk.hemmen@gmail.com>
2014-02-11 21:49:35 -0600

    - Pound pick field to data model (Still need to persist model to server)

--- src/root/protected/views/site/pick.php
+++ src/root/protected/views/site/pick.php
@@ -9,7 +9,17 @@ foreach ($picks as $pick) {
 ?>
 <script>
 loserpool.controller('PickPageCtrl', ['$scope', function($scope) {
-    $scope.picks        = <?php echo CJSON::encode($picks);?>;
+    var pick, team;
+    $scope.picks = <?php echo CJSON::encode($picks);?>;
+    $scope.teams = <?php echo CJSON::encode($teams);?>;
+    for (pick in $scope.picks) {
+        for (team in $scope.teams) {
+            if ($scope.teams[team].id == $scope.picks[pick].teamid) {
+                $scope.picks[pick].team = $scope.teams[team];
+                break;
+            }
+        }
+    }
     console.dir($scope.picks);  // KDHTODO remove
 }]);
 </script>
@@ -31,18 +41,15 @@ loserpool.controller('PickPageCtrl', ['$scope', function($scope) {
                 <tr>
                     <td><?=$week?></td>
                     <td>
-                        <select week="<?=$week?>">
-                            <option value="0">Select Loser...</option>
-                            <?php
-                            // KDHTODO select what the user had
-                            // KDHTODO add a column for the logo
-                            // KDHTODO make a fancier select like the main page, to select a team by logo in a small modal?
-                            // KDHTODO AJAX call to save when the dropdown changes
-                            // KDHTODO disable dropdowns that are locked
-                            foreach ($teams as $team) {
-                                echo "<option value=\"{$team['id']}\"" . ($pickByWeek[$week] == $team['id'] ? ' selected="selected"' : '') . ">{$team['longname']}</option>";
-                            }
-                            ?>
+                        <!--
+                        // KDHTODO select what the user had
+                        // KDHTODO add a column for the logo
+                        // KDHTODO make a fancier select like the main page, to select a team by logo in a small modal?
+                        // KDHTODO AJAX call to save when the dropdown changes
+                        // KDHTODO disable dropdowns that are locked
+                        -->
+                        <select ng-model="picks[<?=$week-1?>].team" ng-options="t.longname for (id, t) in teams">
+                          <option value="">Select Loser...</option>
                         </select>
                     </td>
                     <!-- KDHTODO populate -->
